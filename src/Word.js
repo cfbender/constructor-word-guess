1fa3fd565dd91d31c8cd0867a9362b33e3e21f84
Cody Bender <50554676+cfbender@users.noreply.github.com>
2019-09-19 12:20:32 -0600

    Update Word.js to be concise

--- src/Word.js
+++ src/Word.js
@@ -2,13 +2,12 @@ const Letter = require("./Letter");
 
 class Word {
   constructor(secret) {
-    this.secretArr = secret.split("");
-    this.displayArr = this.secretArr.map(letter => new Letter(letter));
+    this.arr = secret.split("").map(letter => new Letter(letter));
   }
 
   display() {
     let built = "";
-    this.displayArr.forEach(letter => (built += `${letter.toString()} `));
+    this.arr.forEach(letter => (built += `${letter.toString()} `));
     console.log(built);
   }
 }
